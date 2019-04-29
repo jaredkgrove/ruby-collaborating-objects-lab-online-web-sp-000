c050c6bf4267e12eb8a55dff31831c56696b83de
Jared Grove <jaredkgrove@gmail.com>
2019-04-29 01:15:10 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -27,7 +27,7 @@ class Artist
   end
   
   def self.find_or_create_by_name(name)
-    self.all.any?{|artist| artist.name = name} ? self.all.detect{|artist| artist.name = name} : self.new(name).save
+    self.all.any?{|artist| artist.name == name} ? self.all.detect{|artist| artist.name == name} : self.new(name).save
   end
   
   def self.all
